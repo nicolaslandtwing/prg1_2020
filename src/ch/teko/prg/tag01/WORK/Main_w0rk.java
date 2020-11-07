13584307d7eb6b3c866c8609e641e24962c32ba5
Nicolas Landtwing <nicolas.landtwing@edu.teko.ch>
2020-11-07 12:27:28 +0100

    Revert "Work Folder Tag 01"
    This reverts commit 9d47bf5c

---
+++ src/ch/teko/prg/tag01/WORK/Main_w0rk.java
@@ -0,0 +1,23 @@
+package ch.teko.prg.tag01.WORK;
+
+public class Main_w0rk {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World! My first JAVA code ...");
+    }
+}
+
+public class test {
+
+    public static void test1 (string[] args) {
+        //this ist just a test
+
+
+    }
+
+
+}
+
+
+
