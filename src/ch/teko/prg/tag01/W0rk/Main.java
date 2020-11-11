12f457dc8b27fee4df42a89208ecbb64a7ef7bc7
Nicolas Landtwing <nicolas.landtwing@edu.teko.ch>
2020-11-11 19:59:17 +0100

    Work Folder Lösungen PKW

--- src/ch/teko/prg/tag01/W0rk/Main.java
+++ src/ch/teko/prg/tag01/W0rk/Main.java
@@ -2,12 +2,14 @@ package ch.teko.prg.tag01.W0rk;
 
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args) {
-        Scanner myObj = new Scanner (System.in);
-        System.out.println("Enter Username: ");
-        String userName=myObj.nextLine ();
-        System.out.println ("Username ist: " + userName);
+public class Main {
+    public static void Pkw(String[] args) {
+        PWK pkw1 = new PWK("Wartburg", "353", 50);
+        PWK pkw2 = new PWK("VW", "Lupo", 50);
+        PWK pkw3 = new PWK("Chevrolet", "Belair", 250);
+        System.out.println("PKW1:", pkw1.getmarke());
+        System.out.println("PKW2:", pkw2.getmarke());
+        System.out.println("PKW3:", pkw3.getmarke());
 
     }
 
---
+++ src/ch/teko/prg/tag01/W0rk/Pkw.java
@@ -0,0 +1,43 @@
+package ch.teko.prg.tag01.W0rk;
+
+public class Pkw {
+    //Instanzvariabeln
+
+    private String marke;
+    private String modell;
+    private int ps;
+
+    //Konstruktor
+    public Pkw(String marke, String modell, int ps) {
+        this.marke  = marke;
+        this.modell = modell;
+        this.ps     = ps;
+
+    }
+
+    public String getMarke() {
+        return marke;
+    }
+
+    public String getModell() {
+        return modell;
+    }
+
+    public void getPs() {
+        this.ps = ps;
+    }
+
+    public void setMarke(String marke) {
+        this.marke = marke;
+    }
+
+    public void setModell(String modell) {
+        this.modell = modell;
+    }
+
+    public void setps(int ps){
+        this.ps = ps;
+    }
+
+
+}
