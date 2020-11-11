19ed5494630fba94a7f8c3b4f9a53205968b425e
Nicolas Landtwing <nicolas.landtwing@edu.teko.ch>
2020-11-11 19:07:28 +0100

    Work Folder Tag 2

---
+++ src/ch/teko/prg/tag01/W0rk/Main.java
@@ -0,0 +1,14 @@
+package ch.teko.prg.tag01.W0rk;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner (System.in);
+        System.out.println("Enter Username: ");
+        String userName=myObj.nextLine ();
+        System.out.println ("Username ist: " + userName);
+
+    }
+
+}
