7620133515c4c2be076ed4a635d19d66a75a8754
Nicolas Landtwing <nicolas.landtwing@edu.teko.ch>
2020-11-14 12:24:02 +0100

    Work Folder Lösungen Pkw

---
+++ src/ch/teko/prg/tag02/W0rk/Adresse.java
@@ -0,0 +1,48 @@
+package ch.teko.prg.tag02.W0rk;
+
+public class Adresse {
+    private String strasse;
+    private int nummer;
+    private int plz;
+    private String wohnort;
+
+
+    public Adresse(String strasse, int nummer, int plz, String wohnort){
+        this.strasse=strasse;
+        this.nummer=nummer;
+        this.plz=plz;
+        this.wohnort=wohnort;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public void setNummer(int nummer) {
+        this.nummer = nummer;
+    }
+
+    public int getPlz() {
+        return plz;
+    }
+
+    public void setPlz(int plz) {
+        this.plz = plz;
+    }
+
+    public String getWohnort() {
+        return wohnort;
+    }
+
+    public void setWohnort(String wohnort) {
+        this.wohnort = wohnort;
+    }
+}
